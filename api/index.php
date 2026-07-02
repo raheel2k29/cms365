cfb99dad805d59ce2b4b7a7292c0990c6b60586a
Raheel <raheel2k29@gmail.com>
2026-07-02 05:18:43 +0500

    Override storage path for Vercel

--- api/index.php
+++ api/index.php
@@ -1,1 +1,19 @@
-<?php require __DIR__ . "/../public/index.php";
+<?php
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+require __DIR__.'/../vendor/autoload.php';
+
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Vercel read-only filesystem fix
+$app->useStoragePath('/tmp/storage');
+
+$app->handleRequest(Request::capture());
