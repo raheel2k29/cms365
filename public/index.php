3df1e6353d28444546cf9eea4cf4bf272cbce104
Raheel <raheel2k29@gmail.com>
2026-07-02 05:24:31 +0500

    Use public/index.php for Vercel routing fix

--- public/index.php
+++ public/index.php
@@ -5,16 +5,33 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
+
+if (isset($_ENV['VERCEL'])) {
+    $storagePath = '/tmp/storage';
+    $directories = [
+        $storagePath,
+        $storagePath . '/app',
+        $storagePath . '/framework',
+        $storagePath . '/framework/cache',
+        $storagePath . '/framework/cache/data',
+        $storagePath . '/framework/sessions',
+        $storagePath . '/framework/views',
+        $storagePath . '/logs'
+    ];
+    foreach ($directories as $dir) {
+        if (!is_dir($dir)) {
+            mkdir($dir, 0777, true);
+        }
+    }
+    $app->useStoragePath($storagePath);
+}
 
 $app->handleRequest(Request::capture());
