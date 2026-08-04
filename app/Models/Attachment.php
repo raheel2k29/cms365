5d1625890757738d1015e2fd41950d2f04b7ead7
Raheel <raheel2k29@gmail.com>
2026-08-04 05:32:50 +0500

    Fix Attachment missing email relationship for fallback

--- app/Models/Attachment.php
+++ app/Models/Attachment.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Attachment extends Model
 {
     protected $guarded = [];
+
+    public function quote()
+    {
+        return $this->belongsTo(Quote::class);
+    }
+
+    public function email()
+    {
+        return $this->belongsTo(Email::class);
+    }
 }
